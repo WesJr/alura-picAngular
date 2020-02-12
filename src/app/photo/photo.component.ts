90a0261d91334f0b25eaa41dd70a877d4ad68e83
Wesley Junior de Souza <wesley.souza@castgroup.com.br>
2020-02-12 12:07:02 -0300

    Criação primeiro componente

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent{
+
+    title = 'aluraPic';
+  description = "Leão";
+  url='C:\Users\f964352\DEV\alura\angular\alura-picAngular\src\assets\leao.jpg'
+}
